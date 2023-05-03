a5ff08cd3623e9a207b4d89ec08f97c679887bd9
Teri Phillips <phillips.terianne@gmail.com>
2023-05-03 16:33:49 -0400

    error in server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //app.use(express.static('../client/dist'));
-app.use(express.static(__dirname + '/public/dist'));
+app.use(express.static(__dirname + '/client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
